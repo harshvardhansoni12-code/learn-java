27ffa352cfe60bf0a59ece526de7ffb0d8eba9ef
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-08-16 10:10:44 +0530

    added new sum3 function

--- function.java
+++ function.java
@@ -2,26 +2,43 @@ import java.util.Scanner;
 
 public class function {
     public static void main(String[] args){
-     printName(); 
+     float a  = sum1(); 
+     System.out.println("Sum of two numbers is: " + a);
     }
-     /*
-        access modifier returnType nName(parameters){
+     
+       /* access modifier returnType nName(parameters){
             //function body
             return statement;
         }
         */
-        static void printName(){
-        System.out.println("My name is Ankit");
-       }
-    /* static int sum (){
-        Scanner sc  = new Scanner(System.in);
-        System.out.println("Enter first number");
-        int a = sc.nextInt();
-        System.out.println("Enter second number");
-        int b = sc.nextInt();
-        int c = a + b;
-      //  System.out.println("The sum of two numbers is: " + c);
-        return c;   
-       }
-        */ 
+
+
+        // NOTHING IS RETURNED FROM THIS FUNCTION
+        static void sum(){
+          System.out.println("Enter two numbers");
+          Scanner sc = new Scanner(System.in);
+          int a = sc.nextInt();
+          int b = sc.nextInt();
+          int c = a + b;
+          System.out.println("Sum of two numbers is: " + c);
+        }
+
+        // SOMETHING IS RETURNED FROM THIS FUNCTION
+
+        static float sum1(){
+          System.out.println("Enter two numbers");
+          Scanner sc = new Scanner(System.in);
+          float a = sc.nextFloat();
+          float b = sc.nextFloat();
+          float c = a + b;
+          return  c;
+        }
+
+
+        // pass the value to the function
+        static void sum2(int a , int b){
+            int c = a + b;
+            System.out.println("Sum of two numbers is: " + c);
+        }
+
 }
