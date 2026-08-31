4907345a45d0a5ccc6618d43f3c4d53f0d1e6255
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-08-31 23:03:42 +0530

    added array of objects

--- arrayproblem.java
+++ arrayproblem.java
@@ -1,13 +1,10 @@
+import java.util.Arrays;
 public class arrayproblem {
-    //
     public static void main(String[] args){
         // System.out.println(1);
-        // 
         int[] arr = {1,2,3,4,5};
-        for (int num : arr) {
             // for every element in array print the element
-            System.out.println(num);
+            System.out.println(Arrays.toString(arr));
             // here num represents elements of the array
-        }
     }
 }
--- inputinarray.java
+++ inputinarray.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 
 public class inputinarray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  
-        System.out.println("length of array is n");
-        int n = sc.nextInt();
+       // Scanner sc = new Scanner(System.in);  
+       // System.out.println("length of array is n");
+//        int n = sc.nextInt();
+
+        // array of primitives
+        /*
         int[] arr = new int[n];
         for (int i = 0 ; i < n ; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println(Arrays.toString(arr));
+         */
+        Scanner sc1 = new Scanner(System.in);
+
+        // array of objects
+        String[] str  = new String[4];
+        for ( int i = 0 ; i < str.length ; i++){
+            str[i] = sc1.next();
+        }
+        System.out.println(Arrays.toString(str));
     }
 }
