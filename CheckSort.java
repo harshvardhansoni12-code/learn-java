391fd357f2d2871ce580247482acc5d1746edbb1
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-09-07 02:04:24 +0530

    added checksort

---
+++ CheckSort.java
@@ -0,0 +1,5 @@
+public class CheckSort {
+    public static void main (String[] args) {
+        System.out.println("hello world");
+    }
+}
--- SearchInString.java
+++ SearchInString.java
@@ -10,24 +10,24 @@ public class SearchInString {
         System.out.println(search2(str, target));
     }
 
-    static boolean search(String str, char target){
-        if(str.length() == 0){
+    static boolean search( String str, char target ) {
+        if(str.length() == 0 ) {
             return false;
         }
         for(int i = 0 ; i < str.length() ; i++ ) {
-            if(target == str.charAt(i)){
+            if( target == str.charAt(i) ) {
                 return true;
             }
         }
         return false;   
     }
 
-    static boolean search2(String str ,  char target){
-        if( str.length() == 0){
+    static boolean search2( String str ,  char target ) {
+        if( str.length() == 0 ) {
             return false;
         }
-        for( char ch : str.toCharArray()){
-            if(ch == target){
+        for( char ch : str.toCharArray() ) {
+            if( ch == target ) {
                 return true;
             }   
         }
